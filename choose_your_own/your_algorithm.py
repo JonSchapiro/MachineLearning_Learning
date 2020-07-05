55d702333d5fd31e8a508d7486bb4e949f870acc
Jonathan Schapiro <jschapiro@opentable.com>
2020-07-05 18:11:44 +0100

    random forest tree example

--- choose_your_own/your_algorithm.py
+++ choose_your_own/your_algorithm.py
@@ -1,9 +1,10 @@
-#!/usr/bin/python
+#!/usr/bin/env python
 
 import matplotlib.pyplot as plt
 from prep_terrain_data import makeTerrainData
 from class_vis import prettyPicture
-
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
 features_train, labels_train, features_test, labels_test = makeTerrainData()
 
 
@@ -17,28 +18,16 @@ bumpy_slow = [features_train[ii][1] for ii in range(0, len(features_train)) if l
 
 
 #### initial visualization
-plt.xlim(0.0, 1.0)
-plt.ylim(0.0, 1.0)
-plt.scatter(bumpy_fast, grade_fast, color = "b", label="fast")
-plt.scatter(grade_slow, bumpy_slow, color = "r", label="slow")
-plt.legend()
-plt.xlabel("bumpiness")
-plt.ylabel("grade")
-plt.show()
+
 ################################################################################
 
 
 ### your code here!  name your classifier object clf if you want the 
 ### visualization code (prettyPicture) to show you the decision boundary
+clf = RandomForestClassifier(criterion="entropy", min_samples_split=50)
+clf.fit(features_train, labels_train)
+predictions = clf.predict(features_test)
+print(accuracy_score(predictions, labels_test))
 
+prettyPicture(clf, features_test, labels_test)
 
-
-
-
-
-
-
-try:
-    prettyPicture(clf, features_test, labels_test)
-except NameError:
-    pass
--- svm/svm_author_id.py
+++ svm/svm_author_id.py
@@ -12,6 +12,8 @@ import sys
 from time import time
 sys.path.append("../tools/")
 from email_preprocess import preprocess
+from sklearn.svm import SVC
+from sklearn.metrics import accuracy_score
 
 
 ### features_train and features_test are the features for the training
@@ -19,12 +21,14 @@ from email_preprocess import preprocess
 ### labels_train and labels_test are the corresponding item labels
 features_train, features_test, labels_train, labels_test = preprocess()
 
+features_train = features_train[:int(len(features_train)*.01)] 
+labels_train = labels_train[:int(len(labels_train)*.01)] 
+
+clf = SVC(C=1000,kernel="rbf")
+clf.fit(features_train, labels_train)
+preds = clf.predict([features_test[10], features_test[26], features_test[50]])
+print(preds)
 
 
-
-#########################################################
-### your code goes here ###
-
-#########################################################
 
 
